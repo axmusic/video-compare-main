1442dc5e87bb9872e33d83737d141621b970acbc
axmusic <adarshghagre@gmail.com>
2026-02-03 00:41:43 +0530

    v0.3.2 distribution

--- dist/ue-video-compare.js
+++ dist/ue-video-compare.js
@@ -1,5 +1,5 @@
 /*!
-* UE Video Comparison - v0.3.2
+* UE Video Comparison - v0.3.3
 * Unlimited Elements for Elementor, Adarsh Pawar.
 * Repository: https://github.com/AxMusic/video-compare-main
 */
